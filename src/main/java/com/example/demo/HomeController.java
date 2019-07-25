9ac62794760d9d3c0b00ac6d8fc6b4c98abf0435
nboober <nboober@terpmail.umd.edu>
2019-07-25 09:37:58 -0400

    Implemented user registration in branch springboot_406

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,10 +1,39 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/register", method = RequestMethod.GET)
+    public String showRegistration(Model model){
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping(value = "/register", method=RequestMethod.GET)
+    public String processRegistration(@Valid
+                                          @ModelAttribute("user") User user, BindingResult result, Model model){
+        model.addAttribute("user", user);
+        if (result.hasErrors()){
+            return "registration";
+        }
+        else{
+            userService.saveUser(user);
+            model.addAttribute("message", "User Account Created");
+        }
+        return "index";
+    }
+
 
     @RequestMapping("/")
     public String index(){
